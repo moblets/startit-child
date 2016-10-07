b94d2ef673a960329c3a7f716e7fe2e57f60afca
Eduardo Russo <russoedu@gmail.com>
2016-10-07 07:21:30 -0300

    [master] Blog translations

---
+++ framework/modules/blog/templates/parts/post-info-comments.php
@@ -0,0 +1,1 @@
+<div class="qodef-post-info-comments-holder"><a class="qodef-post-info-comments" href="<?php comments_link(); ?>" target="_self"><?php echo esc_html__('Comentários','startit'); ?></a></div>
